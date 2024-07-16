47b71f0eb1d10d22289e1ff9eb5246fe45d66aeb
altyna <altyna.behtenova@gmail.com>
2024-07-16 19:55:14 +0700

    Add Range Part1 and Range_main

--- Range/src/ru/academits/arefieva/range/Range.java
+++ Range/src/ru/academits/arefieva/range/Range.java
@@ -1,4 +1,36 @@
 package ru.academits.arefieva.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        final double epsilon = 1.0e-10;
+        return number - from >= -epsilon && to - number >= -epsilon;
+    }
 }
--- Range/src/ru/academits/arefieva/range_main/Main.java
+++ Range/src/ru/academits/arefieva/range_main/Main.java
@@ -1,7 +1,34 @@
 package ru.academits.arefieva.range_main;
 
+import ru.academits.arefieva.range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range = new Range(3, 7);
+        System.out.printf("Диапазон от %f до %f%n", range.getFrom(), range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Заданное число (7) попадает в указанный диапазон - " + range.isInside(7));
+        System.out.println();
+
+        range.setFrom(10);
+        range.setTo(15);
+        System.out.printf("Диапазон от %f до %f%n", range.getFrom(), range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Заданное число (25) попадает в указанный диапазон - " + range.isInside(25));
+        System.out.println();
+
+        range.setFrom(0);
+        range.setTo(0);
+        System.out.printf("Диапазон от %f до %f%n", range.getFrom(), range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.println("Заданное число (5.00) попадает в указанный диапазон - " + range.isInside(5));
+        System.out.println();
+
+        range.setFrom(13.56);
+        range.setTo(24.89);
+        System.out.printf("Диапазон от %f до %f%n", range.getFrom(), range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+        System.out.printf("Заданное число (15.44) попадает в указанный диапазон - " + range.isInside(15.44));
+        System.out.println();
     }
 }
