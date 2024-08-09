e9f1e108574bd3c369ba1a5b58090f01f45d5411
altyna <altyna.behtenova@gmail.com>
2024-08-09 22:45:39 +0700

    Add Triangle class

---
+++ Shapes/src/ru/academits/arefieva/shapes/Triangle.java
@@ -0,0 +1,39 @@
+package ru.academits.arefieva.shapes;
+
+public class Triangle extends Shapes {
+    private final double x1;
+    private final double y1;
+    private final double x2;
+    private final double y2;
+    private final double x3;
+    private final double y3;
+
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+    }
+
+    public double getWidth() {
+        return Math.max(Math.max(x1, x2), x3) - Math.min(Math.min(x1, x2), x3);
+    }
+
+    public double getHeight() {
+        return Math.max(Math.max(y1, y2), y3) - Math.min(Math.min(y1, y2), y3);
+    }
+
+    public double getArea() {
+        return 0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+    }
+
+    public double getPerimeter() {
+        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+        double side3 = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
+
+        return side1 + side2 + side3;
+    }
+}
