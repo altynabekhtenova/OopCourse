37d785f68a01a80147fdab9d52be5b6083e2e080
altyna <altyna.behtenova@gmail.com>
2024-08-27 22:26:24 +0700

    Add Main in "Shape" exercise

--- Shapes/src/ru/academits/arefieva/shapes_main/Main.java
+++ Shapes/src/ru/academits/arefieva/shapes_main/Main.java
@@ -1,42 +1,32 @@
 package ru.academits.arefieva.shapes_main;
 
+import ru.academits.arefieva.comparators.FigureAreaComparator;
+import ru.academits.arefieva.comparators.FigurePerimeterComparator;
 import ru.academits.arefieva.shapes.*;
 
 import java.util.Arrays;
 
 public class Main {
-    public static String getMaxAreaFigure(Shapes[] shapes) {
-        double maxArea = shapes[0].getArea();
-        int maxAreaFigureNumber = 0;
+    public static Shape getMaxAreaFigure(Shape[] figures) {
+        Arrays.sort(figures, new FigureAreaComparator().reversed());
 
-        for (int i = 1; i < shapes.length; ++i) {
-            if (shapes[i].getArea() > maxArea) {
-                maxArea = shapes[i].getArea();
-                maxAreaFigureNumber = i;
-            }
-        }
-
-        return shapes[maxAreaFigureNumber].toString();
+        return figures[0];
     }
 
-    public static String getSecondMaxPerimeterFigure(Shapes[] shapes) {
-        Arrays.sort(shapes, new MaxPerimeterComparator().reversed());
+    public static Shape getSecondMaxPerimeterFigure(Shape[] figures) {
+        Arrays.sort(figures, new FigurePerimeterComparator().reversed());
 
-        return shapes[1].toString();
+        return figures[1];
     }
 
     public static void main(String[] args) {
-        Square square1 = new Square(40);
-        Square square2 = new Square(15);
-        Triangle triangle1 = new Triangle(1, 2, 4, 2, 11, 115);
-        Triangle triangle2 = new Triangle(5, 8, -5, 2, 9, 15);
-        Rectangle rectangle1 = new Rectangle(2, 4);
-        Rectangle rectangle2 = new Rectangle(12, 1);
-        Circle circle1 = new Circle(6);
-        Circle circle2 = new Circle(5);
+        Shape[] figures = {new Square(40), new Square(15),
+                new Triangle(1, 2, 4, 2, 11, 115),
+                new Triangle(5, 8, -5, 2, 9, 15),
+                new Rectangle(2, 4), new Rectangle(12, 1),
+                new Circle(600), new Circle(5)};
 
-        Shapes[] shapes = new Shapes[]{square1, square2, triangle1, triangle2, rectangle1, rectangle2, circle1, circle2};
-        System.out.printf("Фигура с максимальной площадью - %s%n", getMaxAreaFigure(shapes));
-        System.out.printf("Фигура со вторым по величине периметром - %s", getSecondMaxPerimeterFigure(shapes));
+        System.out.printf("Фигура с максимальной площадью - %s%n", getMaxAreaFigure(figures));
+        System.out.printf("Фигура со вторым по величине периметром - %s", getSecondMaxPerimeterFigure(figures));
     }
 }
