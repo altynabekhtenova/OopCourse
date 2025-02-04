1f59a0ad0079ca9880405f19f47d2ea71f0fcd44
altyna <altyna.behtenova@gmail.com>
2025-02-04 22:34:39 +0700

    Add second part of exercise "Range"

--- Range/src/ru/academits/arefieva/range/Range.java
+++ Range/src/ru/academits/arefieva/range/Range.java
@@ -33,6 +33,46 @@ public class Range {
         return number <= to && number >= from;
     }
 
+    public Range getIntersection(Range range) {
+        Range intersectionResult;
+
+        if (from >= range.to || to <= range.from) {
+            intersectionResult = null;
+        } else {
+            intersectionResult = new Range(Math.max(from, range.from), Math.min(to, range.to));
+        }
+
+        return intersectionResult;
+    }
+
+    public Range[] getUnion(Range range) {
+        Range[] unionResult;
+
+        if (from > range.to || to < range.from) {
+            unionResult = new Range[]{new Range(from, range.from), new Range(range.from, range.to)};
+        } else {
+            unionResult = new Range[]{(new Range(Math.min(from, range.from), Math.max(to, range.to)))};
+        }
+
+        return unionResult;
+    }
+
+    public Range[] getDifference(Range range) {
+        Range[] differenceResult;
+
+        if (from >= range.from && to <= range.to) {
+            differenceResult = null;
+        } else if (from < range.from && to > range.to) {
+            differenceResult = new Range[]{(new Range(from, range.from)), (new Range(range.to, to))};
+        } else if (to > range.to) {
+            differenceResult = new Range[]{new Range(Math.max(from, range.to), to)};
+        } else {
+            differenceResult = new Range[]{new Range(from, Math.min(range.from, to))};
+        }
+
+        return differenceResult;
+    }
+
     @Override
     public String toString() {
         return "(" + from + ", " + to + ")";
--- Range/src/ru/academits/arefieva/range_main/Main.java
+++ Range/src/ru/academits/arefieva/range_main/Main.java
@@ -1,14 +1,28 @@
 package ru.academits.arefieva.range_main;
 
 import ru.academits.arefieva.range.Range;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(-1.5, 3.7);
-        range.setTo(6.8);
-        System.out.printf("Длина диапазона %s = %.1f%n", range, range.getLength());
+        double[] from = {-4, 5, 6, -9, 9, 10};
+        double[] to = {10, 11, 12, 13, 20, 22};
 
-        double number = 4.5;
-        System.out.printf("Принадлежность числа %.1f к диапазону %s - %b", number, range, range.isInside(number));
+        Range range1 = new Range(0, 13);
+        Range range2 = new Range(from[0], to[0]);
+
+        for (double value : from) {
+            for (double v : to) {
+                range2.setFrom(value);
+                range2.setTo(v);
+
+                System.out.printf("Результат пересечения диапазонов (0.0; 13.0) и (%.1f; %.1f) = %s%n",
+                        value, v, range1.getIntersection(range2));
+                System.out.printf("Результат объединения диапазонов (0.0; 13.0) и (%.1f; %.1f) = %s%n",
+                        value, v, Arrays.toString(range1.getUnion(range2)));
+                System.out.printf("Результат разности диапазонов (0.0; 13.0) и (%.1f; %.1f) = %s%n%n",
+                        value, v, Arrays.toString(range1.getDifference(range2)));
+            }
+        }
     }
 }
