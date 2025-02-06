8c96fadad8141daa07361742c7cd7ddab5abb7be
altyna <altyna.behtenova@gmail.com>
2025-02-06 23:32:02 +0700

    Add corrections of "Range" exercise

--- Range/src/ru/academits/arefieva/range_main/Main.java
+++ Range/src/ru/academits/arefieva/range_main/Main.java
@@ -8,22 +8,25 @@ public class Main {
     public static void main(String[] args) {
         Range range1 = new Range(-10, 19);
 
-        double[] secondRangeFrom = {-4, 5, 6, -9, 9, 10};
-        double[] secondRangeTo = {10, 11, 12, 13, 20, 22};
+        double[] startPoint = {-4, 5, 6, -9, 9, 10};
+        double[] endPoint = {10, 11, 12, 13, 20, 22};
 
-        Range range2 = new Range(secondRangeFrom[0], secondRangeTo[0]);
+        Range range2 = new Range(startPoint[0], endPoint[0]);
 
-        for (double from : secondRangeFrom) {
-            for (double to : secondRangeTo) {
-                range2.setFrom(from);
-                range2.setTo(to);
+        for (double secondRangeFrom : startPoint) {
+            for (double secondRangeTo : endPoint) {
+                range2.setFrom(secondRangeFrom);
+                range2.setTo(secondRangeTo);
 
                 System.out.printf("Результат пересечения диапазонов (%.1f; %.1f) и (%.1f; %.1f) = %s%n",
-                        range1.getFrom(), range1.getTo(), from, to, range1.getIntersection(range2));
+                        range1.getFrom(), range1.getTo(), secondRangeFrom, secondRangeTo,
+                        range1.getIntersection(range2));
                 System.out.printf("Результат объединения диапазонов (%.1f; %.1f) и (%.1f; %.1f) = %s%n",
-                        range1.getFrom(), range1.getTo(), from, to, Arrays.toString(range1.getUnion(range2)));
+                        range1.getFrom(), range1.getTo(), secondRangeFrom, secondRangeTo,
+                        Arrays.toString(range1.getUnion(range2)));
                 System.out.printf("Результат разности диапазонов (%.1f; %.1f) и (%.1f; %.1f) = %s%n%n",
-                        range1.getFrom(), range1.getTo(), from, to, Arrays.toString(range1.getDifference(range2)));
+                        range1.getFrom(), range1.getTo(), secondRangeFrom, secondRangeTo,
+                        Arrays.toString(range1.getDifference(range2)));
             }
         }
     }
